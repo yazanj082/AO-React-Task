2d64973d0291712f218fa7cb4f06050ae4bd1f74
UOPEOPLE\Yazan.Jarrar <yazan.jarrar@uopeople.edu>
2024-05-16 17:26:50 +0300

    adding the shared style for card and adding peoplelist page text

--- src/modules/star-wars/pages/PeopleList/PeopleList.tsx
+++ src/modules/star-wars/pages/PeopleList/PeopleList.tsx
@@ -1,11 +1,17 @@
-import { Box } from "@mui/material";
+import { Link, Stack, Typography } from "@mui/material";
 
+import styles from './PeopleList.module.css'
 
 const PeopleList = () => {
     return (
-        <Box>
-            test
-        </Box>
+        <Stack className={`${styles.peopleList} card`}>
+            <Typography className={styles.peopleListHeader}>
+                This page contains the table that represent the names of starwars characters.
+                The data of the table are retrived using from star war API <Link href="https://swapi.dev/api/people/">Api</Link>.
+                You can check the git repo <Link href="https://github.com/yazanj082/AO-React-Task">AO-React-Task</Link> for source code.
+            </Typography>
+
+        </Stack>
     )
 }
 
